5d7bfcbd41d51bf8973b60175533bed5b8a650e2
Enver Morinaj <emorinaj@agroviva.de>
2021-07-08 11:12:32 +0200

    Update Install EGroupware.sh

--- Install EGroupware.sh
+++ Install EGroupware.sh
@@ -8,15 +8,23 @@ apt install apache2 egroupware-docker -y
 mkdir -p /usr/share/egroupware
 
 
-# scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/cao/  /usr/share/egroupware/cao/
-# scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/attendance/  /usr/share/egroupware/attendance/
-# scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/threecx/  /usr/share/egroupware/threecx/
+STD_MSG="Where is the data located;"
+read sourceEGW
 
-rsync -u -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/usr/share/egroupware/cao/*  /usr/share/egroupware/cao/
-rsync -u -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/usr/share/egroupware/attendance/*  /usr/share/egroupware/attendance/
-rsync -u -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/usr/share/egroupware/threecx/*  /usr/share/egroupware/threecx/
-rsync -u -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/usr/share/egroupware/wiki/*  /usr/share/egroupware/wiki/
+sync_directories()
+{
+    # scp -P 2712 -r root@sourceEGW:/usr/share/egroupware/cao/  /usr/share/egroupware/cao/
+    # scp -P 2712 -r root@sourceEGW:/usr/share/egroupware/attendance/  /usr/share/egroupware/attendance/
+    # scp -P 2712 -r root@sourceEGW:/usr/share/egroupware/threecx/  /usr/share/egroupware/threecx/
+
+    rsync -u -r -v -e 'ssh -p 2712' root@sourceEGW:/usr/share/egroupware/cao/*  /usr/share/egroupware/cao/
+    rsync -u -r -v -e 'ssh -p 2712' root@sourceEGW:/usr/share/egroupware/attendance/*  /usr/share/egroupware/attendance/
+    rsync -u -r -v -e 'ssh -p 2712' root@sourceEGW:/usr/share/egroupware/threecx/*  /usr/share/egroupware/threecx/
+    rsync -u -r -v -e 'ssh -p 2712' root@sourceEGW:/usr/share/egroupware/wiki/*  /usr/share/egroupware/wiki/
 
 
-rsync -u --exclude 'tmp/*' -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/var/lib/egroupware/egw00/*  /var/lib/egroupware/egw00/
-rsync -u --exclude 'tmp/*' -r -v -e 'ssh -p 2712' root@e00.agroviva.net:/var/lib/egroupware/egw03/*  /var/lib/egroupware/egw03/
+    rsync -u --exclude 'tmp/*' -r -v -e 'ssh -p 2712' root@sourceEGW:/var/lib/egroupware/egw00/*  /var/lib/egroupware/egw00/
+    #rsync -u --exclude 'tmp/*' -r -v -e 'ssh -p 2712' root@sourceEGW:/var/lib/egroupware/egw03/*  /var/lib/egroupware/egw03/
+}
+
+sync_directories()
