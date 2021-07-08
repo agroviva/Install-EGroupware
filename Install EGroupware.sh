f8eaae7cf52fa7c45eb0d4b76243c47e358ab2ce
Enver Morinaj <emorinaj@agroviva.de>
2021-07-08 09:59:38 +0200

    Update Install EGroupware.sh

--- Install EGroupware.sh
+++ Install EGroupware.sh
@@ -1,3 +1,13 @@
+echo 'deb http://download.opensuse.org/repositories/server:/eGroupWare/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/server:eGroupWare.list
+sudo apt install gnupg # required, but not installed by apt-key add in Debian 10 
+wget -nv https://download.opensuse.org/repositories/server:eGroupWare/xUbuntu_20.04/Release.key -O - | sudo apt-key add -
+sudo apt update
+sudo apt upgrade -y
+
+apt install apache2 egroupware-docker -y
+mkdir -p /usr/share/egroupware
+
+
 # scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/cao/  /usr/share/egroupware/cao/
 # scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/attendance/  /usr/share/egroupware/attendance/
 # scp -P 2712 -r root@e00.agroviva.net:/usr/share/egroupware/threecx/  /usr/share/egroupware/threecx/
