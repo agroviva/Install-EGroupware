8d0aa467ed03b6555f6749eb7dc720e29aa88ae6
Enver Morinaj <emorinaj@agroviva.de>
2021-12-14 13:54:57 +0100

    Update ReinstallApps.sh

--- ReinstallApps.sh
+++ ReinstallApps.sh
@@ -3,6 +3,8 @@
 cd /usr/share/egroupware
 rm -rf cao/
 rm -rf attendance/
+rm -rf wiki/
 git clone https://github.com/agroviva/cao
 git clone https://github.com/agroviva/attendance
+git clone https://github.com/EGroupware/wiki
 docker-compose -f /etc/egroupware-docker/docker-compose.yml restart egroupware
