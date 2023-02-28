8822f53a978ad854df2501c494f8de588417f2ab
Enver Morinaj <34061524+emorinaj@users.noreply.github.com>
2023-02-28 11:24:42 +0100

    Update ReinstallApps.sh

--- ReinstallApps.sh
+++ ReinstallApps.sh
@@ -3,8 +3,10 @@
 cd /usr/share/egroupware
 rm -rf cao/
 rm -rf attendance/
+rm -rf threecx/
 rm -rf wiki/
 git clone https://github.com/agroviva/cao
 git clone https://github.com/agroviva/attendance
+git clone https://github.com/agroviva/threecx
 git clone https://github.com/EGroupware/wiki
 docker-compose -f /etc/egroupware-docker/docker-compose.yml restart egroupware
