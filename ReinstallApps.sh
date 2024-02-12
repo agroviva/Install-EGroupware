c0fff1c21cf2aebe9eaf555276aaa28e718b49aa
Enver Morinaj <34061524+emorinaj@users.noreply.github.com>
2024-02-12 23:51:15 +0100

    Update ReinstallApps.sh

--- ReinstallApps.sh
+++ ReinstallApps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cd /usr/share/egroupware
+rm -rf agroviva/
 rm -rf cao/
 rm -rf attendance/
 rm -rf threecx/
 rm -rf wiki/
+git clone https://github.com/agroviva/agroviva
 git clone https://github.com/agroviva/cao
 git clone https://github.com/agroviva/attendance
 git clone https://github.com/agroviva/threecx
